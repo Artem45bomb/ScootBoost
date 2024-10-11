e10acbe9467a638e5b0b4dd27d828921f4b12660
Artem45bomb <artemsenkevich348@gmail.com>
2024-10-11 14:31:50 +0300

    update navigation

--- app/src/main/java/com/example/scootboost/routes/auth/AuthRoutes.kt
+++ app/src/main/java/com/example/scootboost/routes/auth/AuthRoutes.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import com.example.scootboost.routes.Login
+import com.example.scootboost.routes.auth.registration.registration
 import com.example.scootboost.screen.auth.LoginScreen
 
 
@@ -11,4 +12,5 @@ fun NavGraphBuilder.authRoutes(navController:NavHostController) {
         composable(Login.route) {
             LoginScreen(navController)
         }
+        registration(navController)
 } 
