def8a2a0c7858dd58bd43ccea6d37775d25ba257
FenixFighter <sashsaw12@gmail.com>
2024-11-02 15:57:21 +0300

    add increase textField

--- app/src/main/java/com/example/scootboost/component/user/techSupport/TechSupportQuestion.kt
+++ app/src/main/java/com/example/scootboost/component/user/techSupport/TechSupportQuestion.kt
@@ -1,31 +1,33 @@
 package com.example.scootboost.component.user.techSupport
 
-
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.scootboost.ui.theme.ScootBoostTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import com.example.scootboost.R
 
 
 @Composable
 fun QuestionItem(modifier: Modifier = Modifier, questionText: String, answerText: String) {
     var isClicked by remember { mutableStateOf(false) };
-    Column(modifier = modifier
-        .padding(horizontal = 8.dp)){
-        TextButton(onClick = {
-            isClicked = !isClicked; },
-            modifier = Modifier.padding(horizontal = 4.dp)){
 
+    Column(modifier = modifier
+        .padding(horizontal = 8.dp))
+    {
+        TextButton(onClick = {isClicked = !isClicked},
+            colors = ButtonDefaults.textButtonColors(
+                contentColor = MaterialTheme.colorScheme.background,
+                disabledContainerColor = MaterialTheme.colorScheme.background
+            ),
+            modifier = Modifier.padding(horizontal = 4.dp)){
             Text(questionText,
                 style = MaterialTheme.typography.displayMedium.copy(
                     MaterialTheme.colorScheme.secondary)
@@ -33,24 +35,24 @@ fun QuestionItem(modifier: Modifier = Modifier, questionText: String, answerText
         }
 
         AnimatedVisibility(isClicked){
-            Text(answerText,
-                modifier = Modifier.padding(top = 4.dp, start = 14.dp, bottom = 4.dp, end = 0.dp),
-                style = MaterialTheme.typography.displayMedium.copy(
-                    MaterialTheme.colorScheme.primary))
+            Column(modifier = Modifier.fillMaxWidth()){
+                Icon(
+                    painterResource(id = R.drawable.btn_show_list),
+                    "btn show list",
+                    tint = MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Text(answerText,
+                    modifier = Modifier.padding(top = 4.dp, start = 14.dp, bottom = 4.dp, end = 0.dp),
+                    style = MaterialTheme.typography.displayMedium.copy(
+                        MaterialTheme.colorScheme.primary)
+                )
+            }
         }
 
         HorizontalDivider(thickness = 1.dp)
 
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewTechSupport() {
-    ScootBoostTheme {
-        QuestionItem(modifier = Modifier.padding(horizontal = 4.dp),"Первый", "Второй")
-    }
-
 }
 
 
--- app/src/main/java/com/example/scootboost/screen/techSupport/TechSupportScreen.kt
+++ app/src/main/java/com/example/scootboost/screen/techSupport/TechSupportScreen.kt
@@ -1,7 +1,10 @@
 package com.example.scootboost.screen.techSupport
 
 
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -31,45 +34,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.scootboost.R
 import com.example.scootboost.component.user.techSupport.QuestionItem
-import com.example.scootboost.data.model.Question
 import com.example.scootboost.data.view.Questions
 import com.example.scootboost.routes.Router
 import com.example.scootboost.routes.RouterType
 import com.example.scootboost.ui.btn.nav.Back
-import com.example.scootboost.ui.theme.ScootBoostTheme
 
 @Router(route = "techSupport", groupId = ["techSupport", "auth"])
 class TechSupportRouter : RouterType
-
-
-@Composable
-@Preview
-fun TechSupportScreenTest() {
-    ScootBoostTheme {
-        val arrayOfQuestions = listOf(
-            Question("Как зарегистрировать устройство в приложении?", "Никак"),
-            Question("Как забронировать устройство?", "Тоже никак"),
-            Question("Можно ли зарегистрироваться без привязки номера телефона?", "Нельзя"),
-            Question("Как проложить навигационный маршрут", "тебе это не нужно"),
-            Question("Как привязать карту к аккаунту?", "только заплатив"),
-            Question(
-                "Что делать, если устройства нет на месте или оно находится в плохом состоянии?",
-                "Ничего"
-            ),
-        )
-        //TechSupportScreen(arrayOfQuestions)
-    }
-
-}
 
 
 @Composable
@@ -87,7 +65,9 @@ fun TechSupportScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Back(navController = navController, modifier = Modifier.align(Alignment.Start).padding(horizontal = 4.dp, vertical = 8.dp))
+                Back(navController = navController, modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(horizontal = 4.dp, vertical = 8.dp))
                 Text(
                     "Техническая поддержка",
                     style = MaterialTheme.typography.titleLarge.copy(
@@ -122,10 +102,12 @@ fun TechSupportScreen(
                 TextField(value = text,
                     onValueChange = { text = it },
                     maxLines = 5,
+
                     modifier = Modifier
+                        .height(IntrinsicSize.Min)
+                        .animateContentSize()
                         .padding(top = 12.dp, bottom = 26.dp, start = 24.dp, end = 24.dp)
                         .fillMaxWidth(fraction = 1f)
-                        .height(IntrinsicSize.Min)
                         .border(
                             width = 1.dp,
                             color = MaterialTheme.colorScheme.secondary,
@@ -139,25 +121,34 @@ fun TechSupportScreen(
                     textStyle = MaterialTheme.typography.displayMedium.copy(color = MaterialTheme.colorScheme.secondary, lineHeight = 23.sp),
                     placeholder = {
                         Text(
-                            text = "Самоката нет на месте",
+                            text = "Ввод",
                             style = MaterialTheme.typography.displayMedium.copy(
                                 color = MaterialTheme.colorScheme.primary
                             )
                         )
                     },
                     trailingIcon = {
-                        Row(modifier = Modifier.fillMaxHeight().padding(top = 0.dp, bottom = 0.dp, end = 0.dp, start = 0.dp), verticalAlignment = Alignment.Bottom){
-                            Icon(
-                                painterResource(id = R.drawable.ic_video),
-                                "camera icon ",
-                                tint = MaterialTheme.colorScheme.primary,
-                                modifier = Modifier.padding(horizontal = 4.dp, vertical = 18.dp)
-                            )
-                            Image(
-                                painter=painterResource(id = R.drawable.ic_confirm),
-                                contentDescription ="confirm input",
-                                modifier = Modifier.padding(horizontal = 8.dp, vertical = 10.dp)
-                            )
+                        AnimatedVisibility(true) {
+                            Row(modifier = Modifier
+                                .fillMaxHeight()
+                                .animateContentSize(animationSpec = spring (
+                                    dampingRatio = Spring.DampingRatioLowBouncy,
+                                    stiffness = Spring.StiffnessLow
+                                ))
+                                .padding(top = 0.dp, bottom = 0.dp, end = 0.dp, start = 0.dp),
+                                verticalAlignment = Alignment.Bottom){
+                                Icon(
+                                    painterResource(id = R.drawable.ic_video),
+                                    "camera icon ",
+                                    tint = MaterialTheme.colorScheme.primary,
+                                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 18.dp)
+                                )
+                                Image(
+                                    painter=painterResource(id = R.drawable.ic_confirm),
+                                    contentDescription ="confirm input",
+                                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 10.dp)
+                                )
+                            }
                         }
                     }
                 )
@@ -175,7 +166,7 @@ fun TechSupportScreen(
                 QuestionItem(
                     modifier = Modifier.padding(vertical = 0.dp),
                     it.headerQuestion,
-                    it.answerQuestion
+                    it.answerQuestion,
                 )
             }
         }
